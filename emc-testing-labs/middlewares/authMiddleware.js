e9076a8650ef0331dcfe7f632dcebbd322643f45
vanshika0602 <166625705+vanshika0602@users.noreply.github.com>
2025-05-13 23:45:51 +0530

    Update and rename validator.js to authMiddleware.js

---
+++ emc-testing-labs/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization; // ✅ lowercase
+
+  console.log(`BackendToken: ${authHeader}`);
+  
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
